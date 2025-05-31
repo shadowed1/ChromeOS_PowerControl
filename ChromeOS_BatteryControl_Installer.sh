825de96a079541d7630cb2752844f89962f8b221
shadowed1 <itszachg@gmail.com>
2025-05-31 15:07:49 -0400

    Update ChromeOS_BatteryControl_Installer.sh

--- ChromeOS_BatteryControl_Installer.sh
+++ ChromeOS_BatteryControl_Installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "Downloading! Requires sudo in crosh to be enabled!"
 sudo mkdir -p /usr/local/bin/ChromeOS_BatteryControl
+echo "Enabling sudo in crosh or run in VT-2 is required!"
 curl -L https://raw.githubusercontent.com/shadowed1/ChromeOS_BatteryControl/refs/heads/main/batterycontrol.sh -o /usr/local/bin/ChromeOS_BatteryControl/batterycontrol.sh
 curl -L https://raw.githubusercontent.com/shadowed1/ChromeOS_BatteryControl/refs/heads/main/batterycontrol_startup.sh -o /usr/local/bin/ChromeOS_BatteryControl/batterycontrol_startup.sh
 curl -L https://raw.githubusercontent.com/shadowed1/ChromeOS_BatteryControl/refs/heads/main/no_turbo_startup.sh -o /usr/local/bin/ChromeOS_BatteryControl/no_turbo_startup.sh
