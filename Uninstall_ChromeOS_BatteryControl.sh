1cd3cf2a3e3ce2e4642f1318f9783f443e0a77c5
shadowed1 <itszachg@gmail.com>
2025-05-31 16:11:57 -0400

    Update Uninstall_ChromeOS_BatteryControl.sh

--- Uninstall_ChromeOS_BatteryControl.sh
+++ Uninstall_ChromeOS_BatteryControl.sh
@@ -8,7 +8,47 @@ if [ -f /etc/init/no_turbo.conf ]; then
 else
     echo "Not found: /etc/init/no_turbo.conf"
 fi
-if [ -f /etc/init/batterycontrol.conf ]; then
+if [ -f /etc/init/batterycontrol.conf ]; thenelif [ "$1" == "uninstall" ]; then
+    echo "Choose an uninstall option:"
+    echo "0: Quit uninstaller"
+    echo "1: Remove no_turbo.conf in /etc/init"
+    echo "2: Remove batterycontrol.conf in /etc/init"
+    echo "3: Remove everything (recommended full uninstall)"
+
+    read -rp "Enter your choice (0-3): " choice
+
+    case "$choice" in
+        0)
+            echo "Uninstall Cancelled."
+            ;;
+        1)
+            if [ -f /etc/init/no_turbo.conf ]; then
+                sudo rm /etc/init/no_turbo.conf && echo "Removed no_turbo.conf"
+            else
+                echo "/etc/init/no_turbo.conf not found."
+            fi
+            ;;
+        2)
+            if [ -f /etc/init/batterycontrol.conf ]; then
+                sudo rm /etc/init/batterycontrol.conf && echo "Removed batterycontrol.conf"
+            else
+                echo "/etc/init/batterycontrol.conf not found."
+            fi
+            ;;
+        3)
+            if [ -x "/usr/local/bin/ChromeOS_BatteryControl/Uninstall_ChromeOS_BatteryControl.sh" ]; then
+                /usr/local/bin/ChromeOS_BatteryControl/Uninstall_ChromeOS_BatteryControl.sh
+            else
+                echo "Uninstall_ChromeOS_BatteryControl.sh not found or is not executable."
+                exit 1
+            fi
+            ;;
+        *)
+            echo "Invalid choice. Uninstall canceled."
+            ;;
+    esac
+    exit 0
+
     sudo rm /etc/init/batterycontrol.conf
     echo "Removed: /etc/init/batterycontrol.conf"
 else
