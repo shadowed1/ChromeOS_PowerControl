e2c9c9588d6da7d9f2b57c047105982034db7467
shadowed1 <itszachg@gmail.com>
2025-05-31 18:29:51 -0400

    Update ChromeOS_BatteryControl_Downloader.sh

--- ChromeOS_BatteryControl_Downloader.sh
+++ ChromeOS_BatteryControl_Downloader.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
-echo "Choose location for ChromeOS_BatteryControl_Installer.sh:"
-read -rp "Enter full path (or press Enter for [/home/chronos/user/tmp/ChromeOS_BatteryControl]): " custom_path
-
-if [ -z "$custom_path" ]; then
-    custom_path="/home/chronos/user/tmp/ChromeOS_BatteryControl"
-fi
-
-mkdir -p "$custom_path"
-echo "Downloading to: $custom_path/ChromeOS_BatteryControl_Installer.sh"
-curl -L https://raw.githubusercontent.com/shadowed1/ChromeOS_BatteryControl/refs/heads/main/ChromeOS_BatteryControl_Installer.sh \
-    -o "$custom_path/ChromeOS_BatteryControl_Installer.sh"
-
-chmod +x "$custom_path/ChromeOS_BatteryControl_Installer.sh"
-echo "Download complete. You can run the installer with VT-2: sudo $custom_path/ChromeOS_BatteryControl_Installer.sh"
+echo "Downloading to: /home/chronos/ChromeOS_BatteryControl_Installer.sh"
+curl -L https://raw.githubusercontent.com/shadowed1/ChromeOS_BatteryControl/refs/heads/main/ChromeOS_BatteryControl_Installer.sh -o /home/chronos/ChromeOS_BatteryControl_Installer.sh
+chmod +x "/home/chronos/ChromeOS_BatteryControl_Installer.sh"
+echo "Download complete. You can run the installer with VT-2: sudo /home/chronos/ChromeOS_BatteryControl_Installer.sh"
