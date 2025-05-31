a16b6e16fa99363ba6d6ad0581c813ec49cc1f95
shadowed1 <itszachg@gmail.com>
2025-05-31 15:06:10 -0400

    Update ChromeOS_BatteryControl_Installer.sh

--- ChromeOS_BatteryControl_Installer.sh
+++ ChromeOS_BatteryControl_Installer.sh
@@ -4,3 +4,4 @@ sudo mkdir -p /usr/local/bin/ChromeOS_BatteryControl
 curl -L https://raw.githubusercontent.com/shadowed1/ChromeOS_BatteryControl/refs/heads/main/batterycontrol.sh -o /usr/local/bin/ChromeOS_BatteryControl/batterycontrol.sh
 curl -L https://raw.githubusercontent.com/shadowed1/ChromeOS_BatteryControl/refs/heads/main/batterycontrol_startup.sh -o /usr/local/bin/ChromeOS_BatteryControl/batterycontrol_startup.sh
 curl -L https://raw.githubusercontent.com/shadowed1/ChromeOS_BatteryControl/refs/heads/main/no_turbo_startup.sh -o /usr/local/bin/ChromeOS_BatteryControl/no_turbo_startup.sh
+curl -L https://raw.githubusercontent.com/shadowed1/ChromeOS_BatteryControl/refs/heads/main/Uninstall_ChromeOS_BatteryControl.sh -o /usr/local/bin/ChromeOS_BatteryControl/Uninstall_ChromeOS_BatteryControl.sh
