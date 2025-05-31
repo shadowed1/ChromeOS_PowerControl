43da28575e23aca3b521c852a1c604b31b7423b0
shadowed1 <itszachg@gmail.com>
2025-05-30 23:45:04 -0400

    Update ChromeOS_BatteryControl_Downloader.sh

--- ChromeOS_BatteryControl_Downloader.sh
+++ ChromeOS_BatteryControl_Downloader.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 mkdir -p /home/chronos/ChromeOS_BatteryControl
-echo "Downloading ChromeOS_BatteryControl to /home/chronos/ChromeOS_BatteryControl!"
 curl -L https://raw.githubusercontent.com/shadowed1/ChromeOS_BatteryControl/refs/heads/main/batterycontrol.sh -o /home/chronos/ChromeOS_BatteryControl/batterycontrol.sh
 curl -L https://raw.githubusercontent.com/shadowed1/ChromeOS_BatteryControl/refs/heads/main/installer.sh -o /home/chronos/ChromeOS_BatteryControl/installer.sh
 echo "ChromeOS_BatteryControl requires running installer.sh downloaded in ~/tmp/ChromeOS_BatteryControl as root user in VT-2 console to finish installing.
