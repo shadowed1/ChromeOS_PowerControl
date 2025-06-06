2ca0411713ef9a812c9ec4b5551f6c70c65d2243
shadowed1 <itszachg@gmail.com>
2025-06-06 02:36:59 -0400

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,3 +1,5 @@
+# config.sh
+
 if [ -z "$INSTALL_DIR" ]; then
     INSTALL_DIR="/usr/local/bin/ChromeOS_PowerControl"
 fi
@@ -9,30 +11,22 @@ CHARGER_PATH="/sys/class/power_supply/CROS_USBPD_CHARGER0/online"
 BATTERY_PATH="/sys/class/power_supply/BAT0/capacity"
 BATTERY_CONFIG_FILE="$INSTALL_DIR/.batterycontrol_config"
 
-# Fancontrol defaults (prefixed FAN_)
-DEFAULT_FAN_MIN_TEMP=48
-DEFAULT_FAN_MAX_TEMP=81
-DEFAULT_FAN_MIN_FAN=0
-DEFAULT_FAN_MAX_FAN=100
-DEFAULT_FAN_SLEEP_INTERVAL=3
-DEFAULT_FAN_STEP_UP=20
-DEFAULT_FAN_STEP_DOWN=1
+DEFAULT_MIN_TEMP=48
+DEFAULT_MAX_TEMP=81
+DEFAULT_MIN_FAN=0
+DEFAULT_MAX_FAN=100
+DEFAULT_SLEEP_INTERVAL=3
+DEFAULT_STEP_UP=20
+DEFAULT_STEP_DOWN=1
 
 ZONE_PATH="/sys/class/thermal/thermal_zone0/temp"
 RUN_FLAG_FAN="$INSTALL_DIR/.fan_curve_running"
-PID_FILE_FAN="$INSTALL_DIR/.fancontrol.pid"
+PID_FILE="$INSTALL_DIR/.fancontrol.pid"
 MONITOR_PID_FILE_FAN="$INSTALL_DIR/powercontrol_tail_fan_monitor.pid"
-
-    # Powercontrol defaults (prefixed POWER_)
-    DEFAULT_POWER_MIN_TEMP=60
-    DEFAULT_POWER_MAX_TEMP=86
-    DEFAULT_POWER_MIN_PERF_PCT=50
-    DEFAULT_POWER_MAX_PERF_PCT=100
-    MAX_TEMP_LIMIT=90
 
 USER_HOME="/home/chronos"
 RUN_FLAG="$INSTALL_DIR/.powercontrol_enabled"
-PID_FILE_POWER="$INSTALL_DIR/.powercontrol_pid"
+PID_FILE="$INSTALL_DIR/.powercontrol_pid"
 NO_TURBO_BACKUP="$INSTALL_DIR/.no_turbo_backup"
 
 CPU_VENDOR=$(grep -m1 'vendor_id' /proc/cpuinfo | awk '{print $3}' || echo "unknown")
@@ -42,96 +36,87 @@ IS_AMD=0
 IS_INTEL=0
 IS_ARM=0
 
-# Fancontrol variables
-FAN_MIN_TEMP=""
-FAN_MAX_TEMP=""
-FAN_MIN_FAN=""
-FAN_MAX_FAN=""
-FAN_SLEEP_INTERVAL=""
-FAN_STEP_UP=""
-FAN_STEP_DOWN=""
+DEFAULT_MAX_TEMP=86
+DEFAULT_MAX_PERF_PCT=100
+DEFAULT_MIN_TEMP=60
+DEFAULT_MIN_PERF_PCT=50
+MAX_TEMP_LIMIT=90
 
-# Powercontrol variables
-POWER_MAX_TEMP=""
-POWER_MAX_PERF_PCT=""
-POWER_MIN_TEMP=""
-POWER_MIN_PERF_PCT=""
+MAX_TEMP=""
+MAX_PERF_PCT=""
+MIN_TEMP=""
+MIN_PERF_PCT=""
 
+# Load configuration for battery, fan, and CPU settings
 load_config() {
-    if [ -f "$CONFIG_FILE" ]; then
-        source "$CONFIG_FILE"
+    # Load Battery Control configuration
+    if [ -f "$BATTERY_CONFIG_FILE" ]; then
+        source "$BATTERY_CONFIG_FILE"
     else
-        POWER_MAX_TEMP=$DEFAULT_POWER_MAX_TEMP
-        POWER_MAX_PERF_PCT=$DEFAULT_POWER_MAX_PERF_PCT
-        POWER_MIN_TEMP=$DEFAULT_POWER_MIN_TEMP
-        POWER_MIN_PERF_PCT=$DEFAULT_POWER_MIN_PERF_PCT
-        save_config
+        CHARGE_MAX=$DEFAULT_CHARGE_MAX
+        CHARGE_MIN=$DEFAULT_CHARGE_MIN
+        save_battery_config
     fi
-}
 
-    load_fan_config
-    load_power_config
-
-    validate_power_config
-}
-
-load_fan_config() {
     if [ -f "$INSTALL_DIR/.fancontrol_config" ]; then
         source "$INSTALL_DIR/.fancontrol_config"
     else
-        FAN_MIN_TEMP=$DEFAULT_FAN_MIN_TEMP
-        FAN_MAX_TEMP=$DEFAULT_FAN_MAX_TEMP
-        FAN_MIN_FAN=$DEFAULT_FAN_MIN_FAN
-        FAN_MAX_FAN=$DEFAULT_FAN_MAX_FAN
-        FAN_SLEEP_INTERVAL=$DEFAULT_FAN_SLEEP_INTERVAL
-        FAN_STEP_UP=$DEFAULT_FAN_STEP_UP
-        FAN_STEP_DOWN=$DEFAULT_FAN_STEP_DOWN
+        MIN_TEMP=$DEFAULT_MIN_TEMP
+        MAX_TEMP=$DEFAULT_MAX_TEMP
+        MIN_FAN=$DEFAULT_MIN_FAN
+        MAX_FAN=$DEFAULT_MAX_FAN
+        SLEEP_INTERVAL=$DEFAULT_SLEEP_INTERVAL
+        STEP_UP=$DEFAULT_STEP_UP
+        STEP_DOWN=$DEFAULT_STEP_DOWN
         save_fan_config
     fi
-}
 
-load_power_config() {
     if [ -f "$CONFIG_FILE" ]; then
         source "$CONFIG_FILE"
     else
-        POWER_MAX_TEMP=$DEFAULT_POWER_MAX_TEMP
-        POWER_MAX_PERF_PCT=$DEFAULT_POWER_MAX_PERF_PCT
-        POWER_MIN_TEMP=$DEFAULT_POWER_MIN_TEMP
-        POWER_MIN_PERF_PCT=$DEFAULT_POWER_MIN_PERF_PCT
-        save_power_config
+        MAX_TEMP=$DEFAULT_MAX_TEMP
+        MAX_PERF_PCT=$DEFAULT_MAX_PERF_PCT
+        MIN_TEMP=$DEFAULT_MIN_TEMP
+        MIN_PERF_PCT=$DEFAULT_MIN_PERF_PCT
+        save_config
     fi
+
+    validate_config
 }
 
+# Save configurations for battery, fan, and power control settings
 save_battery_config() {
     echo "CHARGE_MAX=${CHARGE_MAX:-$DEFAULT_CHARGE_MAX}" > "$BATTERY_CONFIG_FILE"
     echo "CHARGE_MIN=${CHARGE_MIN:-$DEFAULT_CHARGE_MIN}" >> "$BATTERY_CONFIG_FILE"
 }
 
 save_fan_config() {
-    echo "FAN_MIN_TEMP=${FAN_MIN_TEMP:-$DEFAULT_FAN_MIN_TEMP}" > "$INSTALL_DIR/.fancontrol_config"
-    echo "FAN_MAX_TEMP=${FAN_MAX_TEMP:-$DEFAULT_FAN_MAX_TEMP}" >> "$INSTALL_DIR/.fancontrol_config"
-    echo "FAN_MIN_FAN=${FAN_MIN_FAN:-$DEFAULT_FAN_MIN_FAN}" >> "$INSTALL_DIR/.fancontrol_config"
-    echo "FAN_MAX_FAN=${FAN_MAX_FAN:-$DEFAULT_FAN_MAX_FAN}" >> "$INSTALL_DIR/.fancontrol_config"
-    echo "FAN_SLEEP_INTERVAL=${FAN_SLEEP_INTERVAL:-$DEFAULT_FAN_SLEEP_INTERVAL}" >> "$INSTALL_DIR/.fancontrol_config"
-    echo "FAN_STEP_UP=${FAN_STEP_UP:-$DEFAULT_FAN_STEP_UP}" >> "$INSTALL_DIR/.fancontrol_config"
-    echo "FAN_STEP_DOWN=${FAN_STEP_DOWN:-$DEFAULT_FAN_STEP_DOWN}" >> "$INSTALL_DIR/.fancontrol_config"
+    echo "MIN_TEMP=${MIN_TEMP:-$DEFAULT_MIN_TEMP}" > "$INSTALL_DIR/.fancontrol_config"
+    echo "MAX_TEMP=${MAX_TEMP:-$DEFAULT_MAX_TEMP}" >> "$INSTALL_DIR/.fancontrol_config"
+    echo "MIN_FAN=${MIN_FAN:-$DEFAULT_MIN_FAN}" >> "$INSTALL_DIR/.fancontrol_config"
+    echo "MAX_FAN=${MAX_FAN:-$DEFAULT_MAX_FAN}" >> "$INSTALL_DIR/.fancontrol_config"
+    echo "SLEEP_INTERVAL=${SLEEP_INTERVAL:-$DEFAULT_SLEEP_INTERVAL}" >> "$INSTALL_DIR/.fancontrol_config"
+    echo "STEP_UP=${STEP_UP:-$DEFAULT_STEP_UP}" >> "$INSTALL_DIR/.fancontrol_config"
+    echo "STEP_DOWN=${STEP_DOWN:-$DEFAULT_STEP_DOWN}" >> "$INSTALL_DIR/.fancontrol_config"
 }
 
-save_power_config() {
-    validate_power_config
-    echo "POWER_MAX_TEMP=${POWER_MAX_TEMP:-$DEFAULT_POWER_MAX_TEMP}" > "$CONFIG_FILE"
-    echo "POWER_MAX_PERF_PCT=${POWER_MAX_PERF_PCT:-$DEFAULT_POWER_MAX_PERF_PCT}" >> "$CONFIG_FILE"
-    echo "POWER_MIN_TEMP=${POWER_MIN_TEMP:-$DEFAULT_POWER_MIN_TEMP}" >> "$CONFIG_FILE"
-    echo "POWER_MIN_PERF_PCT=${POWER_MIN_PERF_PCT:-$DEFAULT_POWER_MIN_PERF_PCT}" >> "$CONFIG_FILE"
+save_config() {
+    validate_config
+    echo "MAX_TEMP=${MAX_TEMP:-$DEFAULT_MAX_TEMP}" > "$CONFIG_FILE"
+    echo "MAX_PERF_PCT=${MAX_PERF_PCT:-$DEFAULT_MAX_PERF_PCT}" >> "$CONFIG_FILE"
+    echo "MIN_TEMP=${MIN_TEMP:-$DEFAULT_MIN_TEMP}" >> "$CONFIG_FILE"
+    echo "MIN_PERF_PCT=${MIN_PERF_PCT:-$DEFAULT_MIN_PERF_PCT}" >> "$CONFIG_FILE"
 }
 
-validate_power_config() {
-    if [[ -z "$POWER_MAX_TEMP" ]]; then POWER_MAX_TEMP=$DEFAULT_POWER_MAX_TEMP; fi
-    if [[ -z "$POWER_MAX_PERF_PCT" ]]; then POWER_MAX_PERF_PCT=$DEFAULT_POWER_MAX_PERF_PCT; fi
-    if [[ -z "$POWER_MIN_TEMP" ]]; then POWER_MIN_TEMP=$DEFAULT_POWER_MIN_TEMP; fi
-    if [[ -z "$POWER_MIN_PERF_PCT" ]]; then POWER_MIN_PERF_PCT=$DEFAULT_POWER_MIN_PERF_PCT; fi
+# Validate configuration values to make sure defaults are set if not defined
+validate_config() {
+    if [[ -z "$MAX_TEMP" ]]; then MAX_TEMP=$DEFAULT_MAX_TEMP; fi
+    if [[ -z "$MAX_PERF_PCT" ]]; then MAX_PERF_PCT=$DEFAULT_MAX_PERF_PCT; fi
+    if [[ -z "$MIN_TEMP" ]]; then MIN_TEMP=$DEFAULT_MIN_TEMP; fi
+    if [[ -z "$MIN_PERF_PCT" ]]; then MIN_PERF_PCT=$DEFAULT_MIN_PERF_PCT; fi
 }
 
+# Function to detect the CPU type (Intel, AMD, ARM)
 detect_cpu_type() {
     case "$CPU_VENDOR" in
         GenuineIntel)
@@ -157,6 +142,7 @@ detect_cpu_type() {
 }
 
 load_config
+
 
 export USER_HOME
 export CONFIG_FILE
