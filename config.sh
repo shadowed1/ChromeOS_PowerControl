4df634d5bfeff11357f5643dadbfa52e5db3c746
shadowed1 <itszachg@gmail.com>
2025-06-07 01:34:57 -0400

    Update config.sh

--- config.sh
+++ config.sh
@@ -19,6 +19,30 @@ SLEEP_INTERVAL=3
 STEP_UP=20
 STEP_DOWN=1
 
+detect_cpu_type() {
+    case "$CPU_VENDOR" in
+        GenuineIntel)
+            IS_INTEL=1
+            if [ -f "/sys/devices/system/cpu/intel_pstate/max_perf_pct" ]; then
+                PERF_PATH="/sys/devices/system/cpu/intel_pstate/max_perf_pct"
+                TURBO_PATH="/sys/devices/system/cpu/intel_pstate/no_turbo"
+            fi
+            ;;
+        AuthenticAMD)
+            IS_AMD=1
+            if [ -f "/sys/devices/system/cpu/amd_pstate/max_perf_pct" ]; then
+                PERF_PATH="/sys/devices/system/cpu/amd_pstate/max_perf_pct"
+            else
+                PERF_PATH="/sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq"
+            fi
+            ;;
+        *)
+            IS_ARM=1
+            PERF_PATH="/sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq"
+            ;;
+    esac
+}
+
 # Default Settings for Control        
 
 # Service startup flags
@@ -45,6 +69,7 @@ RUN_FLAG_POWER="$INSTALL_DIR/.powercontrol_enabled"
 PID_FILE_BATTERY="$INSTALL_DIR/.batterycontrol_pid"
 PID_FILE_POWER="$INSTALL_DIR/.powercontrol_pid"
 PID_FILE_FAN="$INSTALL_DIR/.fan_curve_pid"
+MONITOR_POWER_PID_FILE="$INSTALL_DIR/.powercontrol_tail_fan_monitor.pid"
 
 
 export STARTUP_BATTERYCONTROL
