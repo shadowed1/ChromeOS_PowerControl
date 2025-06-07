62f7f69c883e507e5540d1efe7ca9961f9fafbc2
shadowed1 <itszachg@gmail.com>
2025-06-07 04:36:15 -0400

    Update ChromeOS_PowerControl_Downloader.sh

--- ChromeOS_PowerControl_Downloader.sh
+++ ChromeOS_PowerControl_Downloader.sh
@@ -3,5 +3,8 @@ echo "Beta"
 echo "Downloading to: /home/chronos/user/tmp/ChromeOS_PowerControl_Installer.sh"
 curl -L https://raw.githubusercontent.com/shadowed1/ChromeOS_PowerControl/beta/ChromeOS_PowerControl_Installer.sh -o /home/chronos/user/tmp/ChromeOS_PowerControl_Installer.sh
 echo "Download complete. You can run the installer with VT-2 after moving it to an executable location:"
+echo ""
 echo "sudo mv /home/chronos/user/tmp/ChromeOS_PowerControl_Installer.sh /usr/local/bin"
+echo ""
 echo "sudo bash /usr/local/bin/ChromeOS_PowerControl_Installer.sh"
+echo ""
