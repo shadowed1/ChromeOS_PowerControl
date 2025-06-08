41b9c0e2a1bcac2d687eb02edd286a7902cf3a1a
shadowed1 <itszachg@gmail.com>
2025-06-07 22:10:52 -0400

    Update ChromeOS_PowerControl_Downloader.sh

--- ChromeOS_PowerControl_Downloader.sh
+++ ChromeOS_PowerControl_Downloader.sh
@@ -1,6 +1,38 @@
 #!/bin/bash
-echo "Downloading to: /home/chronos/ChromeOS_PowerControl_Installer.sh"
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+BLUE=$(tput setaf 4)
+MAGENTA=$(tput setaf 5)
+CYAN=$(tput setaf 6)
+BOLD=$(tput bold)
+RESET=$(tput sgr0)
+echo ""
+echo ""
+echo ""
+echo "           ${RED}████████████${RESET}           "
+echo "       ${RED}████${RESET}        ${RED}████${RESET}       "
+echo "     ${RED}██${RESET}              ${YELLOW}██${RESET}     "
+echo "   ${GREEN}██${RESET}     ${BLUE}██████${RESET}     ${YELLOW}██${RESET}   "
+echo "  ${GREEN}██${RESET}     ${BLUE}████████${RESET}     ${YELLOW}██${RESET}  "
+echo "  ${GREEN}██${RESET}     ${BLUE}████████${RESET}     ${YELLOW}██${RESET}  "
+echo "   ${GREEN}██${RESET}     ${BLUE}██████${RESET}     ${YELLOW}██${RESET}   "
+echo "     ${GREEN}██${RESET}              ${YELLOW}██${RESET}     "
+echo "       ${GREEN}████${RESET}        ${YELLOW}████${RESET}       "
+echo "           ${GREEN}████████████${RESET}           "
+echo ""
+echo "     ${BOLD}${GREEN}Chrome${RESET}${BOLD}${RED}OS${RESET}${BOLD}${YELLOW}_${RESET}${BOLD}${BLUE}PowerControl${RESET}"
+echo ""
+echo ""
+echo ""
+echo ""
+echo "${CYAN}${BOLD}Downloading to: /home/chronos/ChromeOS_PowerControl_Installer.sh $RESET"
 curl -L https://raw.githubusercontent.com/shadowed1/ChromeOS_PowerControl/main/ChromeOS_PowerControl_Installer.sh -o /home/chronos/ChromeOS_PowerControl_Installer.sh
-echo "Download complete. You can run the installer with VT-2 after moving it to an executable location:"
-echo "sudo mv /home/chronos/ChromeOS_PowerControl_Installer.sh /usr/local/bin"
+echo "${GREEN}${BOLD}Download complete. Run the installer with VT-2 or (enable sudo in crosh)."
+echo "Move it to an executable location or run these commands with VT-2:$RESET"
+echo ""
+echo "${BOLD}sudo mv /home/chronos/ChromeOS_PowerControl_Installer.sh /usr/local/bin"
+echo ""
 echo "sudo bash /usr/local/bin/ChromeOS_PowerControl_Installer.sh"
+echo "$RESET"
+echo ""
