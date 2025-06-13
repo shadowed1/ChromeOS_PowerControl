ff96ae33e9948eca507f7546a43732601f818840
shadowed1 <itszachg@gmail.com>
2025-06-13 07:21:18 -0400

    Update ChromeOS_PowerControl_Downloader.sh

--- ChromeOS_PowerControl_Downloader.sh
+++ ChromeOS_PowerControl_Downloader.sh
@@ -22,12 +22,14 @@ echo "       ${GREEN}████${RESET}        ${YELLOW}████${RESET}  
 echo "           ${GREEN}████████████${RESET}           "
 echo ""
 echo "     ${BOLD}${GREEN}Chrome${RESET}${BOLD}${RED}OS${RESET}${BOLD}${YELLOW}_${RESET}${BOLD}${BLUE}PowerControl${RESET}"
+echo ""
 echo "Features:"
 echo "${CYAN}PowerControl: Control CPU clockspeed in relation to temperature; enabling lower temperatures under load and longer battery life."${RESET}
 echo "${GREEN}BatteryControl: Control battery charging limit instead of relying on Adaptive Charging to maximize battery longevity.${RESET}"
 echo "${YELLOW}FanControl: Control fan curve in relation to temperature with built-in hysteresis and 0% RPM mode."
 echo "${MAGENTA}GPUControL: Control GPU clockspeed below its default maximum; enabling lower temperatures and longer battery life when rendering 3D content.${RESET}"
 echo "${BLUE}SleepControl: Control how long ChromeOS can be idle before being able to sleep."
+echo ""
 echo "${CYAN}${BOLD}Downloading to: /home/chronos/ChromeOS_PowerControl_Installer.sh $RESET"
 curl -L https://raw.githubusercontent.com/shadowed1/ChromeOS_PowerControl/beta/ChromeOS_PowerControl_Installer.sh -o /home/chronos/ChromeOS_PowerControl_Installer.sh
 echo "${GREEN}${BOLD}Download complete. Run the installer with VT-2 or enable sudo in crosh."
