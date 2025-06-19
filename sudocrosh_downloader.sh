d154e3edfd4566c56d73d73f9e00ad49b08d2899
shadowed1 <itszachg@gmail.com>
2025-06-19 17:07:26 -0400

    Update sudocrosh_downloader.sh

--- sudocrosh_downloader.sh
+++ sudocrosh_downloader.sh
@@ -8,13 +8,28 @@ CYAN=$(tput setaf 6)
 BOLD=$(tput bold)
 RESET=$(tput sgr0)
 echo "${CYAN}${BOLD}Downloading to: /home/chronos/sudocrosh.sh $RESET"
+curl -L https://raw.githubusercontent.com/shadowed1/ChromeOS_PowerControl/main/minioverride.c -o /home/chronos/user/MyFiles/
 curl -L https://raw.githubusercontent.com/shadowed1/ChromeOS_PowerControl/main/sudocrosh.sh -o /home/chronos/sc.sh
-echo "${GREEN}${BOLD}Download complete. Open VT-2, login as root, and run the following commands to enable sudo in crosh shell:${RESET}"
-echo "${BOLD}sudo mkdir -p /usr/local/bin"
 echo ""
-echo "sudo mv /home/chronos/user/MyFiles/sc.sh /usr/local/bin"
+echo "${BOLD}Copy paste the commands below:${RESET}"
 echo ""
-echo "sudo bash /usr/local/bin/sc.sh"
+echo "Back in ${YELLOW}Crostini${RESET}, copy paste:"
+echo "${CYAN}gcc ~/minioverride.c -o ~/minioverride.so -shared${CYAN}"
+echo ""
+echo "In the **Chrome Files** app, copy ${CYAN}minioverride.so${RESET} from My Files/Linux Files into My Files/"
+echo ""
+echo ""In ${RED}VT-2 as root${RESET} run:
+echo "${BOLD}"
+echo "sudo mkdir -p /usr/local/bin"
+echo ""
+echo "mv /home/chronos/user/MyFiles/minioverride.so /usr/local/bin/"
+echo ""
+echo "mv /home/chronos/user/MyFiles/sc.sh /usr/local/bin"
+echo ""
+echo "chmod +x /usr/local/bin/minioverride.so"
+
+echo ""
+echo "bash /usr/local/bin/sc.sh"
 echo ""
 echo "reboot${RESET}"
 echo ""
