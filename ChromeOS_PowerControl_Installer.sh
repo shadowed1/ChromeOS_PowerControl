cfa08729d5c501cb9a742bff0b721ff2f2b13257
shadowed1 <itszachg@gmail.com>
2025-08-18 20:43:06 -0400

    Update ChromeOS_PowerControl_Installer.sh

--- ChromeOS_PowerControl_Installer.sh
+++ ChromeOS_PowerControl_Installer.sh
@@ -775,8 +775,7 @@ fi
 if [[ "$SHOW_SLEEPCONTROL_NOTICE" -eq 1 ]]; then
 echo "${BLUE}╔════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗"
 echo "║  ${RESET}${BLUE}${BOLD}SleepControl Notice:${RESET}${BLUE}                                                                                              ║"
-echo "║  Overrides default ChromeOS sleep behavior except for lid control - Adjust settings above to preference            ║"
-echo "║  Lid Sleep logic is available. Disable ChromeOS 'Sleep when lid is closed' option to customize.                    ║"
+echo "║  Relies on default ChromeOS sleep behavior - Adjust settings above to preference.                                  ║"
 echo "╚════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╝${RESET}"
 echo ""
 fi
