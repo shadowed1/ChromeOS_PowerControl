5b64a8e56c9bcc034e1fb45692d6345b789e572b
shadowed1 <itszachg@gmail.com>
2025-08-29 01:10:17 -0400

    Update ChromeOS_PowerControl_Installer.sh

--- ChromeOS_PowerControl_Installer.sh
+++ ChromeOS_PowerControl_Installer.sh
@@ -618,11 +618,17 @@ start_component_now() {
 echo "${BLUE}Stopping any existing components of ChromeOS_PowerControl (in case of reinstall)${RESET}"
 sudo ectool backlight 1 >/dev/null 2>&1
 sudo bash "$INSTALL_DIR/powercontrol" stop 2>/dev/null
+echo ""
 sudo bash "$INSTALL_DIR/powercontrol" max_perf_pct 100 2>/dev/null
+echo ""
 sudo bash "$INSTALL_DIR/powercontrol" no_turbo 0 2>/dev/null
+echo ""
 sudo bash "$INSTALL_DIR/batterycontrol" stop 2>/dev/null
+echo ""
 sudo bash "$INSTALL_DIR/fancontrol" stop 2>/dev/null
+echo ""
 sudo bash "$INSTALL_DIR/gpucontrol" restore 2>/dev/null
+echo ""
 sudo bash "$INSTALL_DIR/sleepcontrol" stop 2>/dev/null
 sleep 1
 echo ""
