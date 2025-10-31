88deed7595a3f52f0433bb9f027ae54976a10dc7
shadowed1 <itszachg@gmail.com>
2025-10-31 14:14:34 -0400

    Update Reinstall_ChromeOS_PowerControl.sh

--- Reinstall_ChromeOS_PowerControl.sh
+++ Reinstall_ChromeOS_PowerControl.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
 YELLOW=$(tput setaf 3)
@@ -7,8 +8,10 @@ MAGENTA=$(tput setaf 5)
 CYAN=$(tput setaf 6)
 BOLD=$(tput bold)
 RESET=$(tput sgr0)
+
 INSTALL_DIR="@INSTALL_DIR@"
 CONFIG_FILE="$INSTALL_DIR/config.sh"
+
 echo "Downloading to:${CYAN} /home/chronos/ChromeOS_PowerControl_Installer.sh $RESET"
 bash <(curl -s "https://raw.githubusercontent.com/shadowed1/ChromeOS_PowerControl/main/ChromeOS_PowerControl_Downloader.sh?$(date +%s)")
 echo "Running commands:"
@@ -19,4 +22,3 @@ sudo mv /home/chronos/ChromeOS_PowerControl_Installer.sh /usr/local/bin
 echo "sudo mv /home/chronos/ChromeOS_PowerControl_Installer.sh /usr/local/bin"
 echo "sudo bash /usr/local/bin/ChromeOS_PowerControl_Installer.sh"
 sudo bash /usr/local/bin/ChromeOS_PowerControl_Installer.sh
-exit 0
