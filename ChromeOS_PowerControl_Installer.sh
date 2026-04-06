64e69768152de2e51280102e8418abe1071fef91
shadowed1 <itszachg@gmail.com>
2026-04-06 00:20:55 -0400

    Update ChromeOS_PowerControl_Installer.sh

--- ChromeOS_PowerControl_Installer.sh
+++ ChromeOS_PowerControl_Installer.sh
@@ -270,7 +270,27 @@ if [[ -n "$CHARD_ROOT" ]]; then
     echo "${GREEN}Downloading powercontrol-gui to: $CHARD_ROOT ${RESET}"
     sudo -E curl -fsSL "https://raw.githubusercontent.com/shadowed1/ChromeOS_PowerControl/main/gui.py" -o "$CHARD_ROOT/bin/powercontrol-gui" 2>/dev/null
     sudo chmod +x "$CHARD_ROOT/bin/powercontrol-gui" 2>/dev/null
+    sudo mkdir -p "$CHARD_ROOT/usr/share/applications/" "$CHARD_ROOT/usr/share/icons/hicolor/48x48/apps/"
+        cat <<'EOF' | sudo tee "$CHARD_ROOT/usr/share/applications/powercontrol-gui.desktop" > /dev/null
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=PowerControl
+Comment=Get the power to control your CPU, Battery, Fan Curve, GPU, and Sleep for ChromeOS!
+Exec=/bin/powercontrol-gui
+Icon=powercontrol
+Terminal=false
+Categories=Utility;System;
+StartupNotify=true
+EOF
+    sudo curl -Ls https://github.com/shadowed1/ChromeOS_PowerControl/blob/main/icons/powercontrol_200p.png?raw=true -o "$CHARD_ROOT/usr/share/icons/hicolor/48x48/apps/powercontrol.png"
 fi
+
+echo "if [[ -n "${CHARD_ROOT:-}" ]]; then"
+echo "    sudo -E curl -fsSL "https://raw.githubusercontent.com/shadowed1/ChromeOS_PowerControl/main/gui.py" -o "$CHARD_ROOT/bin/powercontrol-gui" 2>/dev/null"
+echo "    sudo chmod +x "$CHARD_ROOT/bin/powercontrol-gui" 2>/dev/null"
+echo
+echo
 
 mkdir -p "$CONFIG_DIR"
 sudo cp $INSTALL_DIR/config.sh $INSTALL_DIR/config.sh.bak 2>/dev/null
