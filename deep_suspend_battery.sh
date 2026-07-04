1afc117e399f49fdd10565dc8fe766facb66b06e
shadowed1 <22551299+shadowed1@users.noreply.github.com>
2026-07-04 06:59:02 -0400

    Update deep_suspend_battery.sh

--- deep_suspend_battery.sh
+++ deep_suspend_battery.sh
@@ -3,8 +3,12 @@
 
 LOG_FILE="/var/log/sleepcontrol.log"
 RED=$'\033[31m'
-BLUE=$'\033[34m'
+GREEN=$'\033[32m'
 YELLOW=$'\033[33m'
+BLUE=$'\033[34m'
+MAGENTA=$'\033[35m'
+CYAN=$'\033[36m'
+BOLD=$'\033[1m'
 RESET=$'\033[0m'
 
 set_sleep_mode() {
