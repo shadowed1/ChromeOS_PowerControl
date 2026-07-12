5edd35e52c8635fe20af0a4d7032c0201e57b031
shadowed1 <22551299+shadowed1@users.noreply.github.com>
2026-07-12 15:49:54 -0400

    Create powercontrol_gui_chard.sh

---
+++ bin/powercontrol_gui_chard.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+yay -S --noconfirm python-cairo
+sudo -E curl -fsSL https://raw.githubusercontent.com/shadowed1/ChromeOS_PowerControl/main/gui.py -o /bin/powercontrol-gui 2>/dev/null
+sudo chmod +x /bin/powercontrol-gui 2>/dev/null
+sudo mkdir -p /usr/share/applications/ /usr/share/icons/hicolor/48x48/apps/
+cat <<'EOF' | sudo tee /usr/share/applications/powercontrol-gui.desktop > /dev/null
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=PowerControl
+Comment=Get the power to control your CPU, Battery, Fan Curve, GPU, and Sleep for ChromeOS! 
+Exec=/bin/powercontrol-gui
+Icon=powercontrol
+Terminal=false
+Categories=Utility;System; 
+StartupNotify=true
+EOF
+sudo curl -Ls https://github.com/shadowed1/ChromeOS_PowerControl/blob/main/icons/powercontrol_200p.png?raw=true -o /usr/share/icons/hicolor/48x48/apps/powercontrol.png
