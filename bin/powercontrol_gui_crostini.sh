2055505bed5dc592a4c7d54841ccf480727225ef
shadowed1 <22551299+shadowed1@users.noreply.github.com>
2026-07-12 15:49:19 -0400

    Create powercontrol_gui_crostini.sh

---
+++ bin/powercontrol_gui_crostini.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+sudo apt update
+sudo apt install gedit gnome-themes-extra gnome-icon-theme -y
+sudo apt install python3-gi gir1.2-gtk-3.0 python3-gi-cairo -y
+sudo curl -fsSL https://raw.githubusercontent.com/shadowed1/ChromeOS_PowerControl/main/gui.py -o /bin/powercontrol-gui 2>/dev/null
+sudo chmod +x /bin/powercontrol-gui 2>/dev/null
+alias powercontrol-gui='sudo -E powercontrol-gui' 
+
+sudo mkdir -p /usr/share/applications/ /usr/share/icons/hicolor/48x48/apps/
+
+cat <<'EOF' | sudo tee /usr/share/applications/powercontrol-gui.desktop > /dev/null
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=PowerControl
+Comment=Get the power to control your CPU, Battery, Fan Curve, GPU, and Sleep for ChromeOS! 
+Exec=/bin/powercontrol-gui
+Icon=powercontrol
+Terminal=false
+Categories=Utility;System; 
+StartupNotify=true
+EOF
+
+sudo curl -Ls https://github.com/shadowed1/ChromeOS_PowerControl/blob/main/icons/powercontrol_200p.png?raw=true -o /usr/share/icons/hicolor/48x48/apps/powercontrol.png
